61d4cd43b919b60147eedc0526af70d3085979bf
smaystr <smaystr@gmail.com>
2019-02-01 17:33:45 +0200

    add bugfix

--- clients/xmlclient.py
+++ clients/xmlclient.py
@@ -8,14 +8,22 @@ q = queue.Queue()
 
 
 class SocketClientThread(threading.Thread):
+
     def __init__(self, _host="10.1.1.12", _port=44000):
+
         threading.Thread.__init__(self)
         self.host = _host
         self.port = _port
         self.sock = socket(AF_INET, SOCK_STREAM)
         self.sock.connect((self.host, self.port))
 
+    @staticmethod
+    def get_text(obj, name):
+        _obj = obj.find(name)
+        return _obj.text if type(_obj) != 'NoneType' else ""
+
     def run(self):
+
         # global results
         size = 65535
         with self.sock as _socket:
@@ -34,36 +42,32 @@ class SocketClientThread(threading.Thread):
                         raise error('Not an XML tree')
 
                     for root in tree.findall('CitiEvent'):
+
                         if root.get('Type') == "Device":
                             for device in root.findall('Device'):
-                                _type = device.find('Type')
-                                if type(_type) != 'NoneType' and _type.text == "Camera":
-                                    _type = _type.text
-                                    _name = device.find('Name').text
-                                    _addr = device.find('Address').text
-                                    _state = device.find('State').text
-                                    _camera_id = device.find('CameraId').text
-                                    _camera_name = device.find('CameraName').text
-                                    # print(_name, _type, _addr, _state, _camera_id, _camera_name)
-                                    q.put([_name, _type, _addr, _state, _camera_id, _camera_name])
+                                _type = self.get_text(device, 'Type')
+                                _name = self.get_text(device, 'Name')
+                                _addr = self.get_text(device, 'Address')
+                                _state = self.get_text(device, 'State')
+                                _camera_id = self.get_text(device, 'CameraId')
+                                _camera_name = self.get_text(device, 'CameraName')
+                                q.put([_name, _type, _addr, _state, _camera_id, _camera_name])
+
                         if root.get('Type') == "Alarm":
                             for alarm in root.findall('Alarm'):
                                 _alarm_id = alarm.get('Id')
-                                _type = alarm.find('Type')
-                                if type(_type) != 'NoneType':
-                                    _type = _type.text
-                                    _camera_name = alarm.find('CameraName').text
-                                    _camera_id = device.find('CameraId').text
-                                    _lane_id = device.find('LaneId').text
-                                    _start_time = device.find('StartTime').text
-                                    _end_time = device.find('EndTime').text
-                                    _comment = device.find('Comment').text
-                                    _video_clip_name = device.find('VideoClipName').text
-                                    _row_ratio = device.find('RowRatio').text
-                                    _column_ratio = device.find('ColumnRatio').text
-                                    q.put([_alarm_id, _type, _comment, _video_clip_name, _camera_id, _camera_name,
-                                           _lane_id, _start_time, _end_time,
-                                           _row_ratio, _column_ratio])
+                                _type = self.get_text(alarm, 'Type')
+                                _camera_name = self.get_text(alarm, 'CameraName')
+                                _camera_id = self.get_text(alarm, 'CameraId')
+                                _lane_id = self.get_text(alarm, 'LaneId')
+                                _start_time = self.get_text(alarm, 'StartTime')
+                                _end_time = self.get_text(alarm, 'EndTime')
+                                _comment = self.get_text(alarm, 'Comment')
+                                _video_clip_name = self.get_text(alarm, 'VideoClipName')
+                                _row_ratio = self.get_text(alarm, 'RowRatio')
+                                _column_ratio = self.get_text(alarm, 'ColumnRatio')
+                                q.put([_alarm_id, _type, _comment, _video_clip_name, _camera_id, _camera_name,
+                                       _lane_id, _start_time, _end_time, _row_ratio, _column_ratio])
                 except (ConnectionResetError, error) as e:
                     # print(e)
                     break
@@ -76,7 +80,7 @@ if __name__ == "__main__":
     while True:
         th = SocketClientThread()
         th.start()
-        echo = ['0', '0', '0', '0', '0', '0']
+        echo = ['0']*11
         results = q.get()
         if results:
             echo = results
